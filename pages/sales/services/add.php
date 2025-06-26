b9e43e4969188b59409fafd679dca453c61534cf
Yashsen1254 <yashsen0402@gmail.com>
2025-06-26 17:30:09 +0530

    Sales/Services Model Added

---
+++ pages/sales/services/add.php
@@ -0,0 +1,131 @@
+<?php
+
+require '../../../includes/init.php';
+include pathOf("includes/header.php");
+include pathOf("includes/salessidebar.php");
+
+?>
+    <body data-sidebar="dark">
+<div id="layout-wrapper">
+
+            
+            <header id="page-topbar">
+                <div class="navbar-header">
+                    <div class="d-flex">
+                        <div class="navbar-brand-box">
+                            <a href="index.html" class="logo logo-dark">
+                                <span class="logo-sm">
+                                    <img src="assets/images/logo.svg" alt="" height="22">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="assets/images/logo-dark.png" alt="" height="17">
+                                </span>
+                            </a>
+
+                            <a href="index.html" class="logo logo-light">
+                                <span class="logo-sm">
+                                    <img src="assets/images/logo-light.svg" alt="" height="22">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="assets/images/logo-light.png" alt="" height="19">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+<div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                    <h4 class="mb-sm-0 font-size-18">Add Service</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item active"></li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+
+                                        <form>
+                                            <div class="mb-3">
+                                                <label for="formrow-firstname-input" class="form-label">Name</label>
+                                                <input type="text" class="form-control" id="Name" name="Name" placeholder="Enter Service Name">
+                                            </div>
+
+                                            <div class="row">
+                                                
+                                                <div class="col-md-6">
+                                                    <div class="mb-3">
+                                                        <label for="formrow-email-input" class="form-label">Description</label>
+                                                        <input type="text" class="form-control" id="Description" name="Description" placeholder="Enter Description Of The Service">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="mb-3">
+                                                        <label for="formrow-password-input" class="form-label">Price</label>
+                                                        <input type="number" class="form-control" id="Price" name="Price" placeholder="Enter Service Price">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-4">
+                                                    <div class="mb-3">
+                                                        <label for="formrow-inputCity" class="form-label">Duration</label>
+                                                        <input type="text" class="form-control" id="Duration" name="Duration" placeholder="Enter Duration Of The Service">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <button type="submit" class="btn btn-primary w-md" onclick="insertData()">Submit</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+            </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>    
+    function insertData() {
+        var Name = $('#Name').val();
+        var Description = $('#Description').val();
+        var Price = $('#Price').val();
+        var Duration = $('#Duration').val();
+
+        $.ajax({
+            url: '../../../api/sales/services/insert.php',
+            type: 'POST',
+            data: {
+                Name: Name,
+                Description: Description,
+                Price: Price,
+                Duration: Duration
+            },
+            success: function(response) {
+                window.location.href = 'index.php';
+            },
+            error: function(response) {
+                alert("Error: ");
+            }
+        })
+    }
+</script>
---
+++ pages/sales/services/index.php
@@ -0,0 +1,138 @@
+<?php
+
+require '../../../includes/init.php';
+include pathOf("includes/header.php");
+include pathOf("includes/salessidebar.php");
+$index = 0;
+$services = select("SELECT * FROM Services");
+
+?>
+    <body data-sidebar="dark">
+
+    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+            
+            <header id="page-topbar">
+                <div class="navbar-header">
+                    <div class="d-flex">
+                        <div class="navbar-brand-box">
+                            <a href="index.html" class="logo logo-dark">
+                                <span class="logo-sm">
+                                    <img src="assets/images/logo.svg" alt="" height="22">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="assets/images/logo-dark.png" alt="" height="17">
+                                </span>
+                            </a>
+
+                            <a href="index.html" class="logo logo-light">
+                                <span class="logo-sm">
+                                    <img src="assets/images/logo-light.svg" alt="" height="22">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="assets/images/logo-light.png" alt="" height="19">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                    <h4 class="mb-sm-0 font-size-18">SERVICES</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <a href="<?= urlOf("pages/sales/services/add.php") ?>" class="btn btn-primary w-md">Add Services</a>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title">
+                                        </h4>
+                                        <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+                                            <thead>
+                                            <tr>
+                                                <th>Sr No.</th>
+                                                <th>Name</th>
+                                                <th>Description</th>
+                                                <th>Price</th>
+                                                <th>Duration</th>
+                                                <th>Update</th>
+                                                <th>Delete</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php foreach($services as $service) : ?>
+                                                <tr>
+                                                    <td><?= $index += 1 ?></td>
+                                                    <td><?= $service["Name"] ?></td>
+                                                    <td><?= $service["Description"] ?></td>
+                                                    <td><?= $service["Price"] ?></td>
+                                                    <td><?= $service["Duration"] ?></td>
+                                                    <form action="update.php" method="POST">
+                                                        <td>
+                                                            <input type="hidden" name="Id" id="Id" value="<?= $service['Id'] ?>">
+                                                            <button type="submit" class="btn btn-primary w-md">UPDATE</button>
+                                                        </td>
+                                                    </form>
+                                                    <td>
+                                                        <button type="submit" class="btn btn-primary w-md" onclick="deleteData(<?= $service['Id'] ?>)">DELETE</button>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+        
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div> <!-- end row -->
+                    </div> <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script>
+    function deleteData(Id) {
+        $.ajax({
+            url: '../../../api/sales/services/delete.php',
+            type: 'POST',
+            data: {
+                Id: Id
+            },
+            success: function(response) {
+                alert("Service Deleted Successfully");
+                location.reload();
+            },
+            error: function(response) {
+                alert("Error")
+                location.reload();
+            }
+        })
+    }
+</script>
