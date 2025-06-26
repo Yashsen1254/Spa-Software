2bfc621b5aa14461564f3246cd716ed5595960fb
Yashsen1254 <yashsen0402@gmail.com>
2025-06-26 17:29:37 +0530

    Includes

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>SPA</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <link rel="shortcut icon" href="<?= urlOf("assets/images/favicon.ico") ?>">
+    <link href="<?= urlOf("assets/css/bootstrap.min.css") ?>" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="<?= urlOf("assets/css/icons.min.css") ?>" rel="stylesheet" type="text/css" />
+    <link href="<?= urlOf("assets/css/app.min.css") ?>" id="app-style" rel="stylesheet" type="text/css" />
+</head>
---
+++ includes/salessidebar.php
@@ -0,0 +1,35 @@
+<div class="vertical-menu">
+
+                <div data-simplebar class="h-100">
+                    <div id="sidebar-menu">
+                        <ul class="metismenu list-unstyled" id="side-menu">
+                            <li class="menu-title" key="t-menu">Menu</li>
+                            <li>
+                                <a href="<?= urlOf("pages/index.php") ?>" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-dashboards">MAIN DASHBOARD</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= urlOf("pages/sales/index.php") ?>" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-dashboards">DASHBOARD</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= urlOf("pages/sales/services/index.php") ?>" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-dashboards">SERVICES</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= urlOf("pages/sales/clients/index.php") ?>" class="has-arrow waves-effect">
+                                    <i class="bx bx-home-circle"></i>
+                                    <span key="t-dashboards">CLIENTS</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                </div>
+            </div>
