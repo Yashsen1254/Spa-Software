bdc68fcd561fa0ec2594a69e3654ed2a6f528781
Yashsen1254 <yashsen0402@gmail.com>
2025-07-02 16:07:41 +0530

    DataBase Updated

--- database/databse.sql
+++ database/databse.sql
@@ -13,7 +13,7 @@ CREATE TABLE `Services` (
     `NoOfAppointments` INT NOT NULL
 );
 
-CREATE TABLE `Clients` (
+CREATE TABLE `Membership` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
     `ServiceId` INT NOT NULL,
     `Name` VARCHAR(100) NOT NULL,
@@ -23,6 +23,7 @@ CREATE TABLE `Clients` (
     `Email` VARCHAR(100) NOT NULL,
     `AmountPaid` DECIMAL(10, 2) NOT NULL,
     `AmountDue` DECIMAL(10, 2) NOT NULL,
+    `TotalAmount` DECIMAL(10, 2) NOT NULL,
     `StartDate` DATE NOT NULL,
     `EndDate` DATE NOT NULL,
     `IsDelete` INT NOT NULL DEFAULT 1,
@@ -51,22 +52,22 @@ CREATE TABLE `Employee` (
 
 CREATE TABLE `Appointments` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `ClientId` INT NOT NULL,
+    `MemberId` INT NOT NULL,
     `EmployeeId` INT NOT NULL,
     `RoomNo` INT NOT NULL,
     `AppointmentDate` DATETIME NOT NULL,
     `InTime` TIME NOT NULL,
     `OutTime` TIME NOT NULL,
     `IsDelete` INT NOT NULL DEFAULT 1,
-    FOREIGN KEY (`ClientId`) REFERENCES `Clients`(`Id`),
+    FOREIGN KEY (`MemberId`) REFERENCES `Membership`(`Id`),
     FOREIGN KEY (`EmployeeId`) REFERENCES `Employee`(`Id`)
 );
 
 CREATE TABLE `Sales` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `ClientId` INT NOT NULL,
+    `MemberId` INT NOT NULL,
     `Amount` DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (`ClientId`) REFERENCES `Clients`(`Id`)
+    FOREIGN KEY (`MemberId`) REFERENCES `Membership`(`Id`)
 );
 
 CREATE TABLE `Expenses` (
@@ -74,6 +75,7 @@ CREATE TABLE `Expenses` (
     `Name` VARCHAR(100) NOT NULL,
     `Description` VARCHAR(255) NOT NULL,
     `Date` DATE NOT NULL,
+    `Volume` VARCHAR(50) NOT NULL,
     `Price` DECIMAL(10, 2) NOT NULL,
     `Quantity` INT NOT NULL,
     `TotalAmount` DECIMAL(10, 2) NOT NULL
@@ -84,4 +86,17 @@ CREATE TABLE `TotalExpenses` (
     `EmployeeTotal` INT NOT NULL,
     `ExpenseTotal` INT NOT NULL,
     `TotalAmount` DECIMAL(10, 2) NOT NULL
-)
+);
+
+CREATE TABLE `Clients` (
+    `Id` INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(100) NOT NULL,
+    `Mobile` INT NOT NULL,
+    `Therapy` VARCHAR(100) NOT NULL,
+    `TherapistName` VARCHAR(100) NOT NULL,
+    `Date` DATE NOT NULL,
+    `InTime` TIME NOT NULL,
+    `OutTime` TIME NOT NULL,
+    `Price` DECIMAL(10, 2) NOT NULL,
+    `Payment` VARCHAR(50) NOT NULL
+);
