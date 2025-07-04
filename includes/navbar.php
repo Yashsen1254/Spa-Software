c34b31488bb9032659efd84abbf2949e8d06427d
Yashsen1254 <yashsen0402@gmail.com>
2025-07-04 17:27:20 +0530

    Nav updated

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,87 +1,58 @@
-            
-            <header id="page-topbar">
-                <div class="navbar-header">
-                    <div class="d-flex">
-                        <!-- LOGO -->
-                        <div class="navbar-brand-box">
-                            <a href="index.html" class="logo logo-dark">
-                                <span class="logo-sm">
-                                    <img src="assets/images/logo.svg" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/logo-dark.png" alt="" height="17">
-                                </span>
-                            </a>
+<!-- ========== Left Sidebar Start ========== -->
+<div class="vertical-menu d-flex flex-column">
 
-                            <a href="index.html" class="logo logo-light">
-                                <span class="logo-sm">
-                                    <img src="assets/images/logo-light.svg" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="assets/images/logo-light.png" alt="" height="19">
-                                </span>
-                            </a>
-                        </div>
+    <!-- SOFTWARE NAME AT THE TOP -->
+    <div style="padding: 15px; text-align: center; background-color: #1f2937; color: #fff; font-weight: bold; font-size: 18px;">
+        Your Software Name
+    </div>
 
-                        <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
-                            <i class="fa fa-fw fa-bars"></i>
-                        </button>
+    <!-- SCROLLABLE MENU -->
+    <div data-simplebar style="flex: 1 1 auto; overflow-y: auto;">
+        <div id="sidebar-menu">
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li>
+                    <a href="<?= urlOf("pages/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-home-circle"></i>
+                        <span key="t-dashboards">Dashboards</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= urlOf("pages/services/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-cog"></i>
+                        <span key="t-dashboards">Services</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= urlOf("pages/membership/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-id-card"></i>
+                        <span key="t-dashboards">Membership</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= urlOf("pages/client/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-user"></i>
+                        <span key="t-dashboards">Clients</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= urlOf("pages/employees/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-group"></i>
+                        <span key="t-dashboards">Employee</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?= urlOf("pages/expenses/index.php") ?>" class="has-arrow waves-effect">
+                        <i class="bx bx-money"></i>
+                        <span key="t-dashboards">Expenses</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 
-                        
-                    </div>
-                </div>
-            </header>
-
-            <!-- ========== Left Sidebar Start ========== -->
-            <div class="vertical-menu">
-
-                <div data-simplebar class="h-100">
-
-                    <!--- Sidemenu -->
-                    <div id="sidebar-menu">
-                        <!-- Left Menu Start -->
-                        <ul class="metismenu list-unstyled" id="side-menu">
-
-                            <li>
-                                <a href="<?= urlOf("pages/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Dashboards</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?= urlOf("pages/services/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Services</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?= urlOf("pages/membership/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Membership</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?= urlOf("pages/client/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Clients</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?= urlOf("pages/employees/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Employee</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?= urlOf("pages/expenses/index.php") ?>" class="has-arrow waves-effect">
-                                    <i class="bx bx-home-circle"></i>
-                                    <span key="t-dashboards">Expenses</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- Sidebar -->
-                </div>
-            </div>
-            <!-- Left Sidebar End -->
-
+    <!-- FIXED CONTACT NUMBER -->
+    <div style="flex-shrink: 0; background-color: #1f2937; color: #fff; text-align: center; padding: 10px;">
+        <small>Contact: <a href="tel:+919876543210" style="color: #fff; text-decoration: none;">+91-9876543210</a></small>
+    </div>
+</div>
+<!-- Left Sidebar End -->
