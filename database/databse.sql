b6b08df6437d39722d24bea6975e3cacf7055e23
Yashsen1254 <yashsen0402@gmail.com>
2025-07-10 19:14:50 +0530

    Database Update

--- database/databse.sql
+++ database/databse.sql
@@ -6,97 +6,104 @@ USE `Spa`;
 
 CREATE TABLE `Services` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(100) NOT NULL,
-    `Description` TEXT NOT NULL,
-    `Price` INT NOT NULL,
-    `Duration` VARCHAR(50) NOT NULL,
-    `NoOfAppointments` INT NOT NULL
+    `Name` VARCHAR(100) NULL,
+    `Description` TEXT NULL,
+    `Price` INT NULL,
+    `Duration` VARCHAR(50) NULL,
+    `NoOfAppointments` INT NULL
 );
 
 CREATE TABLE `Membership` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `ServiceId` INT NOT NULL,
-    `Name` VARCHAR(100) NOT NULL,
-    `Mobile` VARCHAR(15) NOT NULL,
-    `Address` VARCHAR(100) NOT NULL,
-    `Age` INT NOT NULL,
-    `Email` VARCHAR(100) NOT NULL,
-    `AmountPaid` DECIMAL(10, 2) NOT NULL,
-    `AmountDue` DECIMAL(10, 2) NOT NULL,
-    `TotalAmount` DECIMAL(10, 2) NOT NULL,
-    `StartDate` DATE NOT NULL,
-    `EndDate` DATE NOT NULL,
-    `IsDelete` INT NOT NULL DEFAULT 1,
+    `ServiceId` INT NULL,
+    `Name` VARCHAR(100) NULL,
+    `Mobile` VARCHAR(15) NULL,
+    `Address` VARCHAR(100) NULL,
+    `Age` INT NULL,
+    `Email` VARCHAR(100) NULL,
+    `AmountPaid` DECIMAL(10, 2) NULL,
+    `AmountDue` DECIMAL(10, 2) NULL,
+    `TotalAmount` DECIMAL(10, 2) NULL,
+    `StartDate` DATE NULL,
+    `EndDate` DATE NULL,
+    `IsDelete` INT NULL DEFAULT 1,
+    `PaymentMode` VARCHAR(50) NULL,
     FOREIGN KEY (`ServiceId`) REFERENCES `Services`(`Id`)
 );
 
 CREATE TABLE `Employee` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(100) NOT NULL,
-    `Mobile` VARCHAR(15) NOT NULL,
-    `Address` VARCHAR(100) NOT NULL,
-    `Age` INT NOT NULL,
-    `Email` VARCHAR(100) NOT NULL,
-    `RelationName` VARCHAR(100) NOT NULL,
-    `RelationMobile` VARCHAR(15) NOT NULL,
-    `RelationAddress` VARCHAR(100) NOT NULL,
-    `Relation` VARCHAR(100) NOT NULL,
-    `JoiningDate` DATE NOT NULL,
-    `ImageFileName` VARCHAR(255) NOT NULL,
-    `AddharCardImageFileName` VARCHAR(255) NOT NULL,
-    `AddharCardNumber` VARCHAR(20) NOT NULL,
-    `SalaryPaid` DECIMAL(10, 2) NOT NULL,
-    `SalaryPaidDate` DATE NOT NULL,
-    `SalaryDue` DECIMAL(10, 2) NOT NULL
+    `Name` VARCHAR(100) NULL,
+    `Mobile` VARCHAR(15) NULL,
+    `Address` VARCHAR(100) NULL,
+    `Age` INT NULL,
+    `Email` VARCHAR(100) NULL,
+    `RelationName` VARCHAR(100) NULL,
+    `RelationMobile` VARCHAR(15) NULL,
+    `RelationAddress` VARCHAR(100) NULL,
+    `Relation` VARCHAR(100) NULL,
+    `JoiningDate` DATE NULL,
+    `ImageFileName` VARCHAR(255) NULL,
+    `AddharCardImageFileName` VARCHAR(255) NULL,
+    `AddharCardNumber` VARCHAR(20) NULL,
+    `TotalSalary` INT NULL,
+    `SalaryPaidDate` DATE NULL,
+    `GivenSalary` INT NULL,
+    `PaymentMode` VARCHAR(255) NULL
 );
 
 CREATE TABLE `Appointments` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `MemberId` INT NOT NULL,
-    `EmployeeId` INT NOT NULL,
-    `RoomNo` INT NOT NULL,
-    `AppointmentDate` DATETIME NOT NULL,
-    `InTime` TIME NOT NULL,
-    `OutTime` TIME NOT NULL,
-    `IsDelete` INT NOT NULL DEFAULT 1,
+    `MemberId` INT NULL,
+    `EmployeeId` INT NULL,
+    `RoomNo` INT NULL,
+    `Massage` TEXT NULL,
+    `AppointmentDate` DATETIME NULL,
+    `InTime` TIME NULL,
+    `OutTime` TIME NULL,
+    `IsDelete` INT NULL DEFAULT 1,
     FOREIGN KEY (`MemberId`) REFERENCES `Membership`(`Id`),
     FOREIGN KEY (`EmployeeId`) REFERENCES `Employee`(`Id`)
 );
 
 CREATE TABLE `Sales` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `MemberId` INT NOT NULL,
-    `Amount` DECIMAL(10, 2) NOT NULL,
+    `MemberId` INT NULL,
+    `Amount` DECIMAL(10, 2) NULL,
     FOREIGN KEY (`MemberId`) REFERENCES `Membership`(`Id`)
 );
 
 CREATE TABLE `Expenses` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(100) NOT NULL,
-    `Description` VARCHAR(255) NOT NULL,
-    `Date` DATE NOT NULL,
-    `Volume` VARCHAR(50) NOT NULL,
-    `Price` DECIMAL(10, 2) NOT NULL,
-    `Quantity` INT NOT NULL,
-    `TotalAmount` DECIMAL(10, 2) NOT NULL
+    `Name` VARCHAR(100) NULL,
+    `Description` VARCHAR(255) NULL,
+    `Date` DATE NULL,
+    `Volume` VARCHAR(50) NULL,
+    `Price` DECIMAL(10, 2) NULL,
+    `Quantity` INT NULL,
+    `TotalAmount` DECIMAL(10, 2) NULL,
+    `PaymentMode` VARCHAR(50) NULL
+
 );
 
 CREATE TABLE `TotalExpenses` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `EmployeeTotal` INT NOT NULL,
-    `ExpenseTotal` INT NOT NULL,
-    `TotalAmount` DECIMAL(10, 2) NOT NULL
+    `EmployeeTotal` INT NULL,
+    `ExpenseTotal` INT NULL,
+    `TotalAmount` DECIMAL(10, 2) NULL
 );
 
 CREATE TABLE `Clients` (
     `Id` INT PRIMARY KEY AUTO_INCREMENT,
-    `Name` VARCHAR(100) NOT NULL,
-    `Mobile` INT NOT NULL,
-    `Therapy` VARCHAR(100) NOT NULL,
-    `TherapistName` VARCHAR(100) NOT NULL,
-    `Date` DATE NOT NULL,
-    `InTime` TIME NOT NULL,
-    `OutTime` TIME NOT NULL,
-    `Price` DECIMAL(10, 2) NOT NULL,
-    `Payment` VARCHAR(50) NOT NULL
+    `Name` VARCHAR(100) NULL,
+    `Mobile` INT NULL,
+    `Therapy` VARCHAR(100) NULL,
+    `EmployeeId` INT NULL,
+    `Date` DATE NULL,
+    `InTime` TIME NULL,
+    `OutTime` TIME NULL,
+    `Massage` TEXT NULL,
+    `Price` DECIMAL(10, 2) NULL,
+    `PaymentMode` VARCHAR(50) NULL,
+    FOREIGN KEY (`EmployeeId`) REFERENCES `Employee`(`Id`)
 );
